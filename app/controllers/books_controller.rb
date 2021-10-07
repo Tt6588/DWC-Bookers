cb5e6a8ea849a87f8cf842c531e22dbea8e36d93
Tt6588 <tatsusato@gmail.com>
2021-10-07 08:53:57 +0000

    [update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def show
   end
 
   def new
-    @book = Book.new
   end
   
   def create
