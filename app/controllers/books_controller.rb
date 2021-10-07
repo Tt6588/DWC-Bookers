73d6e4e900e0e05bd223e12c3e75b13858cf8c5a
Tt6588 <tatsusato@gmail.com>
2021-10-07 05:47:21 +0000

    [update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,29 @@
 class BooksController < ApplicationController
   def index
-    @books = book.all
+    @books = bookers.all
   end
-
+  
   def show
   end
 
   def new
     @book = Book.new
   end
+  
+  def create
+    book =Book.new(book_params)
+    book.save
+    redirect_to '/top'
+    
+  end
 
   def edit
   end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
