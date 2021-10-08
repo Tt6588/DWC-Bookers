844f627537908cc0bdd2605fb476674b4b7eb57e
Tt6588 <tatsusato1011@gmail.com>
2021-10-08 04:17:26 +0000

    [update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,11 @@ class BooksController < ApplicationController
     
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
   
   private
   def book_params
